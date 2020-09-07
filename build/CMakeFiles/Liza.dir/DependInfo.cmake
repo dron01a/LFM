4c1b2d6d85ef84e42295ead6c52aa858305f472e
ARmodRey <armodrej01@gmail.com>
2020-09-07 19:32:52 +0300

    cmake build

---
+++ build/CMakeFiles/Liza.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/armodrey/Cклад/Liza(file manager)/src/FSTool/file control.cpp" "/media/armodrey/Cклад/Liza(file manager)/build/CMakeFiles/Liza.dir/src/FSTool/file_control.cpp.o"
+  "/media/armodrey/Cклад/Liza(file manager)/src/main.cpp" "/media/armodrey/Cклад/Liza(file manager)/build/CMakeFiles/Liza.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Liza.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Liza.dir/src/FSTool/file_control.cpp.o"
+  "CMakeFiles/Liza.dir/src/main.cpp.o"
+  "Liza"
+  "Liza.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Liza.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
