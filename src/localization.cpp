fa0a976ff2dd3b5067a6f373035b35a6a383d677
ARmodRey <armodrej01@gmail.com>
2021-03-08 20:06:19 +0300

    fix localization

--- src/localization.cpp
+++ src/localization.cpp
@@ -5,11 +5,11 @@ std::string localization(std::string locFile, std::string text){
     XML * loc = loadXML(locXML.buff()); 
     std::string result; 
     int found = 0; // element with <TAG>
-    string_content message(text," \n");
+    string_content message(text," \t\n");
     for (int i = 0; i < message.size(); i++){
         if(message[i].find("<") != std::string::npos && message[i].find(">") != std::string::npos){
             std::string tag = message[i];
-            tag = tag.substr(tag.find("<") + 1, tag.size() - 2);
+            tag = tag.substr(tag.find("<") + 1, tag.find(">") - tag.find("<") -1);
             std::string tagText = loc->getTAG(tag).value;
             message.edit(i,tagText); // replase tag from tag text
         }
