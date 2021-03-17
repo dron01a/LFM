5bf26a530a467a7f54787c07757a48ba74aa6a5a
ARmodRey <armodrej01@gmail.com>
2021-03-18 00:06:34 +0300

    rework command-read system.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ enum _command{
     _move,
     _rename,
     _info,
-    _help
+    _help,
+    _unK_COM
 };
 
 // return commnad
@@ -32,7 +33,6 @@ int main(int argc, char **argv){
     localPath += config.lang;
     message result;  // result of the program
     _command _command = getCommnad(argc,argv); // get curient comand
-
     switch(_command){
         case _help: // get info of commands
             if(config.mode != "log"){
@@ -66,81 +66,24 @@ int main(int argc, char **argv){
             }
             break;
         case _move:
+            printf("move\n");
             if(countArgsCheck(result,argc,4,4)){
+                printf("move\n");
                 move(argv[2],argv[3],result); // move object
             }
             break;
         case _info:
             if(countArgsCheck(result,argc,3) && config.mode != "log"){
                 for(int i = 2; i < argc; i++){
-                    information(argv[2],result); // return information of files
+                    information(argv[i],result); // return information of files
                 }
             }
             break;
+        default:
+        if(config.mode != "log"){
+            result.add("<NON_COMMAND>");
+        }
     }
-
-    // if (strcmp(argv[1],"help") == 0){
-    //     if(config.mode != "log"){
-    //         result.add("<HELP>");
-    //     }
-    // }
-    // else if(strcmp(argv[1],"mfl") == 0){
-    //     if(argc < 3){
-    //         result.add("<WRONG_ARGS>"); // if bad args 
-    //     }
-    //     for(int i = 2; i < argc; i++){
-    //         create("<FILE>", argv[i], result);
-    //     }
-    // }
-    // else if (strcmp(argv[1],"mdr") == 0){
-    //     if(argc < 3){
-    //         result.add("<WRONG_ARGS>"); // if bad args 
-    //     }
-    //     for(int i = 2; i < argc; i++){
-    //         create("<FOLDER>", argv[i],result);
-    //     }
-    // }
-    // else if (strcmp(argv[1],"move") == 0){
-    //     if(argc < 4 || argc > 4 ){
-    //         result.add("<WRONG_ARGS>");
-    //     }
-    //     move(argv[2],argv[3],result);
-    // }
-    // else if(strcmp(argv[1],"rfl") == 0){
-    //     if(argc < 3){
-    //         result.add("<WRONG_ARGS>");
-    //     }    
-    //     for(int i = 2; i < argc; i++){
-    //         destroy("<FILE>", argv[i],result);
-    //     }
-    // }   
-    // else if (strcmp(argv[1],"rdr") == 0){
-    //     if(argc < 3){
-    //         result.add("<WRONG_ARGS>");
-    //     }    
-    //     for(int i = 2; i < argc; i++){
-    //         destroy("<FOLDER>", argv[i],result);
-    //     }
-    // }
-    // else if (strcmp(argv[1],"remove") == 0){
-    //     for(int i = 2; i < argc; i++){
-    //         destroy(argv[i],result);
-    //     }
-    // }
-    // else if (strcmp(argv[1],"rename") == 0){
-    //     if(argc < 4 || argc > 4 ){
-    //         result.add("<WRONG_ARGS>");
-    //     }
-    //     rename(argv[2],argv[3],result);
-    // }
-    // else if(strcmp(argv[1],"info") == 0){
-    //     information(argv[2],result);
-    // }
-    // else{
-    //     if(config.mode != "log"){
-    //         result.add("<NON_COMMAND>");
-    //     }
-    // }
     result.setText(localization(localPath,result.text()));
     if(config.mode == "display"){
         std::cout << result.text() << std::endl;
@@ -153,30 +96,33 @@ _command getCommnad(int argc, char **argv){
     if(strcmp(argv[1],"mfl") == 0){
         return _createFile;
     }
-    if(strcmp(argv[1],"mdr") == 0){
+    else if(strcmp(argv[1],"mdr") == 0){
         return _createFolder;
     }
-    if( strcmp(argv[1],"move")  ){
+    else if(strcmp(argv[1],"move") == 0){
         return _move;
     }
-    if( strcmp(argv[1],"rfl") == 0 || 
+    else if( strcmp(argv[1],"rfl") == 0 || 
         strcmp(argv[1],"rdr") == 0 || 
         strcmp(argv[1],"remove") == 0 ){
         return _remove;
     }
-    if (strcmp(argv[1],"rename") == 0){
+    else if (strcmp(argv[1],"rename") == 0){
         return _rename;
     }
-    if(strcmp(argv[1],"info") == 0){
+    else if(strcmp(argv[1],"info") == 0){
         return _info;
     }
-    if(strcmp(argv[1],"help") == 0){
+    else if(strcmp(argv[1],"help") == 0){
         return _help;
+    }
+    else{
+        return _unK_COM;
     }
 }
 
 bool countArgsCheck(message & _result, int count, int firstLim, int lastLim){
-    if(count < firstLim || (count > lastLim && lastLim != 0 )){
+    if(count < firstLim || (count > lastLim && lastLim != 0)){
         _result.add("<WRONG_ARGS>"); // if bad args 
         return false;
     }
